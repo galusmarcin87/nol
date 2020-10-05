bf3b29500084d772c299ac0677111269b9d0983d
mgalus <mgalus@dreamlab.pl>
2020-10-05 16:10:33 +0200

    fdg

--- web/coordinator.php
+++ web/coordinator.php
@@ -4,10 +4,12 @@
 if($_COOKIE['userToken'] == '12345'){
     header('x-coordinator-set-cookie: userLoggedIn=1');
     header('x-coordinator-set-header: userLoggedIn=1');
+    http_response_code(307);
 }
 
 if($_COOKIE['userToken'] == '67890'){
     header('x-coordinator-set-cookie: userLoggedIn=0');
     header('x-coordinator-set-header: userLoggedIn=0');
+    http_response_code(307);
 }
 

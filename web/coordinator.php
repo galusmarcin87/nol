3daa321800c2854fc1e3ef1400f2ac65bf82faa7
mgalus <mgalus@dreamlab.pl>
2020-10-05 16:35:30 +0200

    asd

--- web/coordinator.php
+++ web/coordinator.php
@@ -1,8 +1,10 @@
 <?php
 // comment out the following two lines when deployed to production
 
-header('x-coordinator-set-cookie: userLoggedIn=1');
-header('x-coordinator-set-header: userLoggedIn=1');
+header('x-coordinator-set-cookie: x-user-logged-in=1');
+header('x-coordinator-set-header: x-user-logged-in=1');
+header('userLoggedIn: 1');
+setcookie('userLoggedIn',1);
 http_response_code(307);
 
 
